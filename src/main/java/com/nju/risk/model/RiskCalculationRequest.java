f52332bdfafd24f5282a6530c04a44ed6f726f15
yilxu <yilxu@ebay.com>
2024-11-11 15:11:11 +0800

    update shapefile location

--- src/main/java/com/nju/risk/model/RiskCalculationRequest.java
+++ src/main/java/com/nju/risk/model/RiskCalculationRequest.java
@@ -1,24 +1,12 @@
 package com.nju.risk.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class RiskCalculationRequest {
     private CompanyInfo companyInfo;
     private StorageTank storageTank;
 
-    // Getters and Setters
-    public CompanyInfo getCompanyInfo() {
-        return companyInfo;
-    }
-
-    public void setCompanyInfo(CompanyInfo companyInfo) {
-        this.companyInfo = companyInfo;
-    }
-
-    public StorageTank getStorageTank() {
-        return storageTank;
-    }
-
-    public void setStorageTank(StorageTank storageTank) {
-        this.storageTank = storageTank;
-    }
 }
